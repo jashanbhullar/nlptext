61c43fda9d571e71f7c07ba0a4be474b23fdd3ba
hell-sing <abhishek_verma@hotmail.com>
2016-10-07 21:47:14 +0530

    Trignometric Functions by Abhishek Verma

---
+++ trignometric.h
@@ -0,0 +1,29 @@
+#include <math.h>
+#include <stdio.h>
+
+double acos(double x);
+//Returns the arc cosine of x in radians.
+
+double asin(double x);
+//Returns the arc sine of x in radians.
+
+double atan(double x);
+//Returns the arc tangent of x in radians.
+
+double cos(double x);
+//Returns the cosine of a radian angle x.
+
+double cosh(double x);
+//Returns the hyperbolic cosine of x.
+
+double sin(double x);
+//Returns the sine of a radian angle x.
+
+double sinh(double x);
+//Returns the hyperbolic sine of x.
+
+double tanh(double x);
+//Returns the hyperbolic tangent of x.
+
+double exp(double x);
+//Returns the value of e raised to the xth power.
