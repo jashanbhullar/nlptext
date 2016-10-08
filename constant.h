705c487794cde5034c76a82556ffcdd2493b2027
hell-sing <abhishek_verma@hotmail.com>
2016-10-08 21:56:59 +0530

    Constant is added in Program by Abhishek Verma

---
+++ constant.h
@@ -0,0 +1,8 @@
+//header file for the constant value used in all programs
+#define pi         3.141592
+#define e          2.718281
+#define zero       0
+#define unity      1
+#define root_two   1.414213
+#define root_three 1.732050
+#define root_five  2.236067
